0057b5cbb20a437e72a062871e2c6ca8c76929d1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:21:49 +0300

    completed 1 exercise

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,13 @@
 // BEGIN (write your solution here)
+const getWeekends = (displayFormat) => {
+    const arrLong = ['saturday', 'sunday'];
+    const arrShort = ['sat', 'sun'];
+    if (displayFormat === 'short') {
+        return arrShort;
+    } else {
+        return arrLong;
+    }
+};
 
+export {getWeekends};
 // END
