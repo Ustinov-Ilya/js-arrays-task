5bc953487934a218b398b8ffd3db913176e733f1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:41:14 +0300

    completed 12 exercise

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,13 @@
 // BEGIN
+export const makeCensored = (text, stopWord) => {
+    const separator = ' ';
+    const result = [];
+    const words = text.split(separator);
+    for (let word of words) {
+        (stopWord.includes(word) === true) ? result.push('$#%!') : result.push(word);
+    }
+    return result.join(separator);
+}
 
+export default makeCensored;
 // END
