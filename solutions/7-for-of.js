6c2054a2fc42bae5635bfe447a3c185c06672fc9
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:31:01 +0300

    completed 7 exercise

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,13 @@
 // BEGIN
+export const calculateAverage = (arr) => {
+    if (arr.length === 0) return null;
+    let sum = 0;
+    const countElements = arr.length;
+    for (let value of arr) {
+        sum += value;
+    }
+    return sum / countElements;
+}
 
+export default calculateAverage;
 // END
