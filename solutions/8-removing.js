f10febff151aab4d57fe1b1c8d514f1e94bffab1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:35:35 +0300

    completed 8 exercise

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,25 @@
 // BEGIN
+export const getSameParity = (arr) => {
+    if (arr.lenght === 0) return [];
+    
+    let isEven = true;
+    const element = Math.abs(arr[0])
 
+    if (element % 2 === 0) {
+        isEven = true;
+    } else {
+        isEven = false;
+    }
+
+    let result = [];
+    for (let value of arr) {
+        if ((Math.abs(value) % 2 === 0) === isEven){
+            result.push(value)
+        }
+    }
+
+    return result;
+}
+
+export default getSameParity;
 // END
