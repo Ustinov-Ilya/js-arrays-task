b502e457d16c49ea7bf65236c7c7361818278802
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:40:39 +0300

    completed 11 exercise

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export const buildDefinitionList = (coll) => {
+    if (coll.length === 0) return '';
+    let pars = [];
+    for (let elements of coll){
+      pars.push(`<dt>${elements[0]}</dt><dd>${elements[1]}</dd>`);
+    }
+    const innerValue = pars.join('');
+    const result = `<dl>${innerValue}</dl>`;
+    return result;
+  }
+  
+  export default buildDefinitionList;
 // END
