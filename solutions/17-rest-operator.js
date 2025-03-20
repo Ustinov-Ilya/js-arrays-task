bda2a2e602899c18cde79dd4932fa9d41d454e82
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:44:12 +0300

    completed 17 exercise

--- solutions/17-rest-operator.js
+++ solutions/17-rest-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
+export const getMax = (arr) => {
+    if (arr.length === 0) return null;
+    let [maxElement, ...elements] = arr;
+    for (const element of elements) {
+        if (element > maxElement) {
+            maxElement = element;
+        }
+    }
+    return maxElement;
+}
 
+export default getMax;
 // END
