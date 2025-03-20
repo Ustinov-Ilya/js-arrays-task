685e16def399639bb6cefd69d8ae05819aa3e00f
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:37:00 +0300

    completed 9 exercise

--- solutions/9-control-statements.js
+++ solutions/9-control-statements.js
@@ -1,3 +1,14 @@
 // BEGIN
+export const getTotalAmount = (money, name) => {
+    let total = 0;
+    for (const currency of money) {
+        if (currency.slice(0,3) === name) {
+            total += Number(currency.slice(3));
+        }
+    }
 
+    return total;
+}
+
+export default getTotalAmount;
 // END
