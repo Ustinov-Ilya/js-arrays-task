cf4e88f8b52bc772ba3261ff2789de33a21e1945
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:44:58 +0300

    completed 18 exercise

--- solutions/18-spread-operator.js
+++ solutions/18-spread-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export const flatten = (arr) => {
+    if (arr.length === 0) return [];
+    let flat = [];
+    for (const element of arr) {
+      if (Array.isArray(element) === true){
+        flat.push(...element);
+      } else {
+        flat.push(element);
+      }
+    }
+    return flat;
+}
 // END
