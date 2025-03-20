a8d616bf645ad8313b7483dd96448fb1725cabc9
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:30:13 +0300

    completed 6 exercise

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,16 @@
 // BEGIN
+export const calculateSum = (arr) => {
+    if (arr.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const currentElement = arr[i];
+        if (currentElement % 3 === 0) {
+            sum += currentElement;
+        }
+    }
+    return sum;
+    
+}
 
+export default calculateSum;
 // END
