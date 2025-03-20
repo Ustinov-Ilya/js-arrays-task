aa2c50a9b4c772b656378b608e5dc691540bd720
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:37:35 +0300

    completed 10 exercise

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,24 @@
 // BEGIN
+export const getSuperSeriesWinner = (arr) => {
+    let victoriesUssr = 0;
+    let victoriesCanada = 0;
+    for (const gameScore of arr) {
+      if (gameScore[0] === gameScore[1]) {
+        continue;
+      } else if (gameScore[0] > gameScore[1]){
+        victoriesCanada += 1;
+      } else {
+        victoriesUssr += 1;
+      }
+    }
+    if (victoriesCanada === victoriesUssr) {
+      return null;
+    } else if (victoriesCanada > victoriesUssr) {
+      return 'canada';
+    } else {
+      return 'ussr';
+    }
+} 
 
+export default getSuperSeriesWinner;
 // END
