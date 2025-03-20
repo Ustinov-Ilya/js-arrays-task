8fb9c8bd03904455da44df29dd4fccb527554bf8
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:27:37 +0300

    completed 4 exercise

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,11 @@
 // BEGIN
+export const addPrefix = (arr, prefix) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(prefix + " " + arr[i]);
+    }
+    return newArr;
+}
 
+export default addPrefix;
 // END
