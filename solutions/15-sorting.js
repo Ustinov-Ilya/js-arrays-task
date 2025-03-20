54fde3e2233f505ea8bb844446c1ebb8dbd0e35c
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:43:01 +0300

    completed 15 exercise

--- solutions/15-sorting.js
+++ solutions/15-sorting.js
@@ -1,3 +1,21 @@
 // BEGIN
+export const bubbleSort = (arr) => {
+    const n = arr.length;
+    for (let j = 1; j < n; j++) {
+        let isSorted = true;
+        for (let i = 0; i < arr.length; i++) {
+            let currentNumber = arr[i];
+            let nextNumber = arr[i + 1];
+            if (arr[i] > arr[i + 1]) {
+                arr[i] = nextNumber;
+                arr[i + 1] = currentNumber;
+                isSorted = false;
+            }
+        }
+        if (isSorted) break;
+    }
+    return arr;
+}
 
+export default bubbleSort;
 // END
