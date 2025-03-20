6590954f34c5fba84ee9f830ffb28444bdc49fb4
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:23:22 +0300

    completed 2 exercise

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,13 @@
 // BEGIN
+const swap = (arr) => {
+    if (arr.length < 2) return arr; 
+    else {
+        const firstSymbol = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = firstSymbol;
+        return arr;
+    }
+}
 
+export {swap};
 // END
