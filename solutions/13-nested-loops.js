7682ec298ab4a25e52cd38568196e86e32405c26
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:41:50 +0300

    completed 13 exercise

--- solutions/13-nested-loops.js
+++ solutions/13-nested-loops.js
@@ -1,5 +1,19 @@
 const _ = require("lodash"); 
 
 // BEGIN
+export const getSameCount = (arr1, arr2) => {
+    let countUniqueValues = 0;
+    const uniqArr1 = _.uniq(arr1);
+    const uniqArr2 = _.uniq(arr2);
+    for (let value1 of uniqArr1){
+        for (let value2 of uniqArr2){
+            if (value1 === value2){
+                countUniqueValues += 1;
+            }
+        }
+    }
+    return countUniqueValues;
+} 
 
+export default getSameCount;
 // END
