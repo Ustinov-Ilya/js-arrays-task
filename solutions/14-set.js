336bb823656ac8d047b7f0f57c4a0dfc25f80fa7
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:42:26 +0300

    completed 14 exercise

--- solutions/14-set.js
+++ solutions/14-set.js
@@ -1,3 +1,14 @@
 // BEGIN
+export const countUniqChars = (text) => {
+    if (text.length === 0) return 0;
+    let count = [];
+    for (let i = 0; i < text.length; i++) {
+        if (count.includes(text[i]) === false) {
+            count.push(text[i]);
+        }
+    }
+    return count.length;
+}
 
+export default countUniqChars;
 // END
