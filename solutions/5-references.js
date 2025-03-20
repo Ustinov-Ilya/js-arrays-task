51ddf1c04637dad84279dc3efc126fee02561356
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-20 13:28:57 +0300

    completed 5 exercise

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,16 @@
 // BEGIN
+export const reverse = (arr) => {
+    let middle = arr.length / 2;
+    if (arr.length % 2 !== 0) {
+        middle = (arr.length - 1) / 2;
+    }
+    for (let i = 0; i < middle; i++) {
+        const currentElement = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = currentElement;
+    }
+    return arr;
+}
 
+export default reverse;
 // END
